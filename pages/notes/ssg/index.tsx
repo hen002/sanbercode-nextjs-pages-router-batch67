eda89bb8363af7d6686b4f89fdc48716bfa52f50
Henry Widodo <henrywidodo6@gmail.com>
2025-06-04 08:29:36 +0700

    Data Fetching StaticProps Pages Router

---
+++ pages/notes/ssg/index.tsx
@@ -0,0 +1,40 @@
+import { GetStaticProps, InferGetStaticPropsType } from "next"
+import Link from "next/link"
+
+type ListNotes = {
+    id: number
+    title: string
+    description: string
+    created_at: string
+    updated_at: string
+
+}
+
+type Notes = {
+    status: string
+    message: string
+    data: ListNotes[]
+}
+
+export const getStaticProps = (async () => {
+    const notes = await fetch('https://service.pace11.my.id/api/notes').then(
+        (rest) => rest.json(),
+    )
+
+    return { props: { notes } }
+}) satisfies GetStaticProps<{ notes: Notes }>
+
+export default function NotesServerPage({ notes,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+    return (
+        <div className="grid grid-cols-4 gap-4">
+            {notes?.data.map((note: ListNotes) => (
+                <Link href={`/notes/server/${note.id}`} key={note.id} className="p-4 bg-white shadow-sm rounded-lg">
+                    <h1>{note.title}</h1>
+                    <p>{note.description}</p>
+                </Link>
+            ))}
+        </div>
+    )
+
+}
