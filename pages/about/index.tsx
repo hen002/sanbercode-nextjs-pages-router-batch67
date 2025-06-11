fe9280b23c4709c2bfe92b1356b3277fba178b29
Henry Widodo <henrywidodo6@gmail.com>
2025-06-11 11:55:08 +0700

    Optimizing Nextjs Pages Router

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,3 +1,24 @@
+// import Image from 'next/image'
+// import HeavyComponent from '@/components/HeavyComponent'
+import dynamic from 'next/dynamic'
+import { useState } from 'react'
+
+const HeavyComponent = dynamic(() => import('@/components/HeavyComponent'),
+    {
+        ssr: false,
+        loading: () => <p>Loading ...</p>
+    }
+)
+
 export default function About() {
-    return (<div>About Page</div>)
+    const [show, setShow] = useState(false)
+
+    return (
+        <div>
+            <h1>About Page</h1>
+            <button onClick={() => setShow(true)}>Show Component</button>
+            {/* <Image src="/blur-bright-bulb-clear-thumbnail.jpg" width="1000" height="500" alt="About Page Image" priority /> */}
+            {show && <HeavyComponent />}
+        </div>
+    )
 }
